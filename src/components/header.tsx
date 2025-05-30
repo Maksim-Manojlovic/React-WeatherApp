c9e666da40e1884ce93dba6d134e6f8bc6301363
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-05-30 11:47:43 +0200

    [ADD] dark and light mode f

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useTheme } from "@/context/theme-provider";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
+
   return (
-    <header className='sticky top-0 z-50 w-full border-bg bg-background-95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60'>
-        <div className='container mx-auto flex h-16 items-center justify-between px-4'>
-            <Link to={"/"}>
-            <img src="/logo.png" alt="Klimate logo" className='h-14' />
-            </Link>
+    <header className="sticky top-0 z-50 w-full border-bg bg-background-95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img
+            src={isDark ? "/logo.png" : "/logo2.png"}
+            alt="Klimate logo"
+            className="h-14"
+          />
+        </Link>
 
-            <div>
-                {/* {search} */}
-                {/* theme toogle */}
-            </div>
+        <div>
+          {/* {search} */}
+          {/* theme toogle */}
+          <div onClick={() => setTheme(isDark ? "light" : "dark")}>toggle</div>
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
