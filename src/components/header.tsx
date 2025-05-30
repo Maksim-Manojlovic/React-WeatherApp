7c55891f89a58d857bcb2e54e56bed488ab24347
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-05-30 12:32:49 +0200

    [ADD] Animation for icons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -20,7 +21,18 @@ const Header = () => {
         <div>
           {/* {search} */}
           {/* theme toogle */}
-          <div onClick={() => setTheme(isDark ? "light" : "dark")}>toggle</div>
+          <div
+            onClick={() => setTheme(isDark ? "light" : "dark")}
+            className={`flex items-center cursor-pointer transition-transform duration-500
+            ${isDark ? "rotate-180" : "rotate-0"}
+            `}
+          >
+            {isDark ? (
+              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all " />
+            ) : (
+              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
+            )}
+          </div>
         </div>
       </div>
     </header>
