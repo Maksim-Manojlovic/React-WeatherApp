3d018b0a586ec97953a1d78031357630c7199b41
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-05-30 11:21:53 +0200

    [ADD] Implementing dark mode theme f

---
+++ src/components/layout.tsx
@@ -0,0 +1,27 @@
+import React, { type PropsWithChildren } from 'react'
+
+ export const Layout = ({children}: PropsWithChildren) => {
+  return (
+    <div className='bg-gradient-to-br from-background to-muted'>
+
+        header
+
+        <main className='min-h-screen container mx-auto px-4 py-8'>
+
+            {children}
+
+        </main>
+      
+      <footer className='border-t backdrop-blur py-12'>
+        <div className='container mx-auto px-4 text-center text-gray-400'>
+            <p>
+                Made by OptAmaze
+            </p>
+        </div>
+      </footer>
+
+    </div>
+  )
+}
+
+export default Layout
