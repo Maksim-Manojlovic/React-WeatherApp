141b39fc0791980794345c95ac53a255914cb823
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-05-31 18:08:14 +0200

    [ADD] hook finished

--- src/hooks/use-geolocation.tsx
+++ src/hooks/use-geolocation.tsx
@@ -26,16 +26,50 @@ export function useGeolocation() {
       return;
     }
 
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLocationData({
-        coordinates: {
-          lat: position.coords.latitude,
-          lon: position.coords.longitude,
-        },
-        error: null,
-        isLoading: false,
-      });
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocationData({
+          coordinates: {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          },
+          error: null,
+          isLoading: false,
+        });
+      },
+      (error) => {
+        let errorMessage: string;
+
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            errorMessage =
+              "Location permission denied. Please enable location access";
+            break;
+
+          case error.POSITION_UNAVAILABLE:
+            errorMessage = "Location information is unavailable.";
+            break;
+
+          case error.TIMEOUT:
+            errorMessage = "Location request timed out.";
+            break;
+          default:
+            errorMessage = "An unknown error occurred.";
+            break;
+        }
+
+        setLocationData({
+          coordinates: null,
+          error: errorMessage,
+          isLoading: false,
+        });
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
   };
 
   useEffect(() => {
