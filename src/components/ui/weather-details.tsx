b5f786245582c8678baac536bf05149809004e9e
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-06-01 10:34:49 +0200

    [ADD] Wind directions const and calc

--- src/components/ui/weather-details.tsx
+++ src/components/ui/weather-details.tsx
@@ -8,6 +8,13 @@ interface WeatherDetailsProps {
 const WeatherDetails = ({ data }: WeatherDetailsProps) => {
   const { wind, main, sys } = data;
 
+  const getWindDirection = (degree: number) => {
+    const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+
+    const index =
+      Math.round(((degree %= 360) < 0 ? degree + 360 : degree) / 45) % 8;
+  };
+
   const formatTime = (timestamp: number) => {
     return format(new Date(timestamp * 1000), "h:mm a");
   };
