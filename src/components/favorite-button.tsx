d9fa7eed7d9d781fb152ee022cd3701161fbb90d
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-06-04 08:32:58 +0200

    [ADD] favorite button

---
+++ src/components/favorite-button.tsx
@@ -0,0 +1,11 @@
+import type { WeatherData } from "@/api/types";
+
+interface FavoriteButtonProps {
+  data: WeatherData;
+}
+
+const FavoriteButton = () => {
+  return <div>FavoriteButton</div>;
+};
+
+export default FavoriteButton;
