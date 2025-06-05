afc19142dd84d60b25a477ff852a7ef2ed9c2943
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2025-06-05 11:19:21 +0200

    [ADD] interface made

--- src/components/ui/favorite.cities.tsx
+++ src/components/ui/favorite.cities.tsx
@@ -1,5 +1,13 @@
 import { useFavorite } from "@/hooks/use-favorite";
-import React from "react";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+
+interface FavoriteCityTabletProps {
+  id: string;
+  name: string;
+  lat: number;
+  lot: number;
+  onRemove: (id: string) => void;
+}
 
 const FavoriteCities = () => {
   const { favorites, removeFavorite } = useFavorite();
@@ -7,7 +15,15 @@ const FavoriteCities = () => {
   if (!favorites.length) {
     return null;
   }
-  return <div>favoriteCities</div>;
+  return (
+    <>
+      <h1 className="text-xl font-bold tracking-tight">Favorites</h1>
+      <ScrollArea className="w-full pb-4">
+        <div className="flex gap-4"></div>
+      </ScrollArea>
+    </>
+  );
 };
+function FavoriteCityTablet(id, name, lat, lon, onRemove) {}
 
 export default FavoriteCities;
